be935a11019378dcd4c55131107ec2bfb908d702
Fabián Díaz González <fabian12ud@gmail.com>
2016-09-04 18:29:27 -0500

    Original

---
+++ EjbHelpPolice/ejbModule/com/edu/udistrital/model/UsuarioModel.java
@@ -0,0 +1,51 @@
+package com.edu.udistrital.model;
+
+import com.edu.udistrital.entity.Perfil;
+import com.edu.udistrital.entity.Persona;
+
+public class UsuarioModel {
+
+	private String contraseña;
+
+	private String descripcion;
+
+	private Perfil perfil;
+
+	private Persona persona;
+
+	public UsuarioModel() {
+		
+	}
+
+	public String getContraseña() {
+		return this.contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Perfil getPerfil() {
+		return this.perfil;
+	}
+
+	public void setPerfil(Perfil perfil) {
+		this.perfil = perfil;
+	}
+
+	public Persona getPersona() {
+		return this.persona;
+	}
+
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+}
